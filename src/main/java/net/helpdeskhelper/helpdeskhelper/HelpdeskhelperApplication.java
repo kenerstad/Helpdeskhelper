9de4133fc78f7386b5ba4f86a17decc285c876ea
kenerstad <kenerstad2@gmail.com>
2018-11-18 12:13:19 +0100

    Initial commit

---
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/HelpdeskhelperApplication.java
@@ -0,0 +1,12 @@
+package net.helpdeskhelper.helpdeskhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskhelperApplication.class, args);
+	}
+}
---
+++ src/test/java/net/helpdeskhelper/helpdeskhelper/HelpdeskhelperApplicationTests.java
@@ -0,0 +1,16 @@
+package net.helpdeskhelper.helpdeskhelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelpdeskhelperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
