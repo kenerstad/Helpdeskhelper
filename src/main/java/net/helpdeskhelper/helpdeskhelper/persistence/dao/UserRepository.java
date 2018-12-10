2999b637320f76fa61b14ae188919414aa9c385c
kenerstad <kenerstad2@gmail.com>
2018-12-10 14:08:32 +0100

    Add @Transactional

--- src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/UserRepository.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/UserRepository.java
@@ -1,15 +1,18 @@
 package net.helpdeskhelper.helpdeskhelper.persistence.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import net.helpdeskhelper.helpdeskhelper.persistence.model.UserModel;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
+
+@Transactional
 public interface UserRepository extends CrudRepository<UserModel, Integer> {
 
 	UserModel findByEmail (String email);
-	UserModel findByUser_Name (String user);
+	UserModel findByUserName (String userName);
 	
 }
