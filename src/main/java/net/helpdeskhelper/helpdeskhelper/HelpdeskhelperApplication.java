6bb6e83d48859427ee3a63cfdf9ac1bb6ed785f2
kenerstad <kenerstad2@gmail.com>
2018-12-10 13:48:27 +0100

    ADD run() override to use generateDefaultroles()&generatedefaultUser()
    addewd run() method override to use generateDefaultroles() & generatedefaultUser() from UserService to generate defaults at startup.

--- src/main/java/net/helpdeskhelper/helpdeskhelper/HelpdeskhelperApplication.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/HelpdeskhelperApplication.java
@@ -1,12 +1,27 @@
 package net.helpdeskhelper.helpdeskhelper;
 
+//# Local
+import net.helpdeskhelper.helpdeskhelper.service.IUserService;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
-public class HelpdeskhelperApplication {
+public class HelpdeskhelperApplication implements CommandLineRunner{
 
+	@Autowired
+	IUserService userService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskhelperApplication.class, args);
 	}
+	
+    @Override
+    public void run(String... args) throws Exception {
+
+    	System.out.println(userService.generateDefaultRoles());
+    	System.out.println(userService.generateDefaultUser());      
+    }   
 }
