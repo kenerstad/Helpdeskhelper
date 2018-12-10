4fdee9fb12232d09c22dff7922a857b609280ed8
kenerstad <kenerstad2@gmail.com>
2018-12-10 14:07:40 +0100

    add findByRoleName() & @Transactional

--- src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/RoleRepository.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/RoleRepository.java
@@ -1,11 +1,13 @@
 package net.helpdeskhelper.helpdeskhelper.persistence.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import net.helpdeskhelper.helpdeskhelper.persistence.model.RoleModel;
 
 
-
+@Transactional
 public interface RoleRepository extends CrudRepository<RoleModel, Integer> {
 
+	public RoleModel findByRoleName(String name);
 }
