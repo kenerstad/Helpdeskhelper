5a4e60c100cf8220205280f7c7051bb1252abb58
kenerstad <kenerstad2@gmail.com>
2018-12-10 19:45:09 +0100

    Add ClientInfoService.java

---
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/service/ClientInfoService.java
@@ -0,0 +1,15 @@
+package net.helpdeskhelper.helpdeskhelper.service;
+
+import net.helpdeskhelper.helpdeskhelper.utils.WebUtils;
+
+//## Spring
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class ClientInfoService {
+
+	
+	
+}
