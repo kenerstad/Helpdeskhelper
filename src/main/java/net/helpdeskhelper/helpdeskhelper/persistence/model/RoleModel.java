44cf59e658224a27f5d477ec0106e4bc96f55287
kenerstad <kenerstad2@gmail.com>
2018-12-10 14:12:23 +0100

    add @manytomany & remove uniqueconstraints

--- src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/model/RoleModel.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/model/RoleModel.java
@@ -1,30 +1,42 @@
 package net.helpdeskhelper.helpdeskhelper.persistence.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
  
 @Entity
-@Table(name = "role", 
-        uniqueConstraints = { 
-                @UniqueConstraint(name = "ROLE_UK", columnNames = "Role_Name") })
+@Table(name = "role")
 
 public class RoleModel {   
+	
     @Id
     @GeneratedValue
-    @Column(name = "Role_Id", nullable = false)
+    @NotNull
     private Long roleId;
  
-    @Column(name = "Role_Name", length = 30, nullable = false)
+    @NotNull
+    @Size(max = 36)
     private String roleName;
  
     
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "roles")
+    private Set<UserModel> users = new HashSet<>();
+    
+    
     // ## GETTERS/SETTERS ##
     
-    public Long getRoleId() {
+	public Long getRoleId() {
         return roleId;
     }
  
@@ -39,5 +51,13 @@ public class RoleModel {
     public void setRoleName(String roleName) {
         this.roleName = roleName;
     }
+    
+    public Set<UserModel> getUsers() {
+  		return users;
+  	}
+
+  	public void setUsers(Set<UserModel> users) {
+  		this.users = users;
+  	}
      
 }
