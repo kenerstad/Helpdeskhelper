c159d85bc1e90a424f6d38626ad7d27c277fe155
kenerstad <kenerstad2@gmail.com>
2018-12-17 23:34:20 +0100

    Add messagesource:vardata

--- src/main/java/net/helpdeskhelper/helpdeskhelper/config/WebConfig.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/config/WebConfig.java
@@ -4,12 +4,17 @@ package net.helpdeskhelper.helpdeskhelper.config;
 import net.helpdeskhelper.helpdeskhelper.web.dto.UserFormDTO;
 import net.helpdeskhelper.helpdeskhelper.validator.UserFormValidator;
 
+//## java
+import java.util.Properties;
+
 //## Spring
 import org.springframework.context.MessageSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -26,9 +31,11 @@ public class WebConfig  implements WebMvcConfigurer {
 	    public MessageSource messageSource() {
 		  
 	        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();	       
-	        messageSource.setBasename("classpath:validation");
+	        messageSource.setBasenames("classpath:validation", 
+	        						   "classpath:vardata");
 	        messageSource.setDefaultEncoding("UTF-8");
 	        return messageSource;
 	    }
+	 
 	    
 }
