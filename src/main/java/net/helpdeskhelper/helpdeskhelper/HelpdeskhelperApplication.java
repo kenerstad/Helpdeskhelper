e52314b6d9b9759dd1236ebbb5c082caf5bf8530
kenerstad <kenerstad2@gmail.com>
2019-01-23 23:25:25 +0100

    ADD issueService.generateIssues() to run()

--- src/main/java/net/helpdeskhelper/helpdeskhelper/HelpdeskhelperApplication.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/HelpdeskhelperApplication.java
@@ -2,6 +2,7 @@ package net.helpdeskhelper.helpdeskhelper;
 
 //# Local
 import net.helpdeskhelper.helpdeskhelper.service.IUserService;
+import net.helpdeskhelper.helpdeskhelper.service.IIssueService;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +15,9 @@ public class HelpdeskhelperApplication implements CommandLineRunner{
 	@Autowired
 	IUserService userService;
 	
+	@Autowired
+	IIssueService issueService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskhelperApplication.class, args);
 	}
@@ -22,6 +26,7 @@ public class HelpdeskhelperApplication implements CommandLineRunner{
     public void run(String... args) throws Exception {
 
     	System.out.println(userService.generateDefaultRoles());
-    	System.out.println(userService.generateDefaultUser());      
+    	System.out.println(userService.generateDefaultUser());    
+    	issueService.generateIssues();
     }   
 }
