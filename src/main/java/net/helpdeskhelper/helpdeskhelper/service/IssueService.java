f9936c3660ff01d056aeafa19a94d104a225a84d
kenerstad <kenerstad2@gmail.com>
2019-04-09 21:21:35 +0200

    ALTER add imports, add default constructor, alter method names, alter method implementations
    modified: getQuestionsByIssue() - implemented issue DTO.
    Added: addCategory(), addIssueToCategory(), generateTemplates(), updateCategory(), deleteCategory(), generateIssue(),
    	updateIssue(), deleteIssue(), generateQuestion(), updateQuestion(), deleteQuestion().
    
    Removed: generateQuestions(), generateCategories(), generateIssues()

--- src/main/java/net/helpdeskhelper/helpdeskhelper/service/IssueService.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/service/IssueService.java
@@ -3,11 +3,17 @@ package net.helpdeskhelper.helpdeskhelper.service;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import javax.validation.constraints.Size;
+
 // ## Spring
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 // ## Project
 import net.helpdeskhelper.helpdeskhelper.persistence.dao.IssueRepository;
 import net.helpdeskhelper.helpdeskhelper.persistence.dao.IssueCategoryRepository;
@@ -16,11 +22,12 @@ import net.helpdeskhelper.helpdeskhelper.persistence.model.Issue;
 import net.helpdeskhelper.helpdeskhelper.persistence.model.Question;
 import net.helpdeskhelper.helpdeskhelper.persistence.model.IssueCategory;
 import net.helpdeskhelper.helpdeskhelper.utils.WebUtils;
-import net.helpdeskhelper.helpdeskhelper.web.dto.CategoryDTO;
-import net.helpdeskhelper.helpdeskhelper.web.dto.IssueDTO;
+import net.helpdeskhelper.helpdeskhelper.web.dto.*;
+
 
 
 @Service
+@Transactional
 public class IssueService implements IIssueService{
 
 	@Autowired
@@ -32,8 +39,11 @@ public class IssueService implements IIssueService{
 	@Autowired
 	IssueCategoryRepository issueCategoryRepo;
 	
-	public void registerNewIssue() {
+	AdminFeedbackDTO feedback;
+	
+	public IssueService() {
 		
+		feedback = new AdminFeedbackDTO();
 	}
 	
 	@SuppressWarnings("null")
@@ -54,8 +64,8 @@ public class IssueService implements IIssueService{
 				
 		for(IssueCategory issueCategory : issueCategories) {
 			CategoryDTO categoryDTO = new CategoryDTO();
+			categoryDTO.setId(issueCategory.getCategoryId());
 			categoryDTO.setCategoryName(issueCategory.getCategoryName());
-			categoryDTO.setCategoryDescription(issueCategory.getCategoryDescription());
 			categories.add(categoryDTO);
 		}
 
@@ -65,31 +75,21 @@ public class IssueService implements IIssueService{
 		 
 	}
 	
-	public String getIssuesByCategory(String category){
-		
-		System.out.println(category);
-		
-		IssueCategory issueCategory = issueCategoryRepo.findByCategoryName(category);
+	public String getIssuesByCategory(Long catId){
+				
+		IssueCategory issueCategory = issueCategoryRepo.findByCategoryId(catId);
 		List<Issue> issues = issueCategory.getIssues();
 		return WebUtils.generateJSON(convertIssuesFromCategoryToString(issues));
 		
 		//return WebUtils.generateJSON(category);
 	}
 	
-	public IssueDTO getIssueQuestions(String issueName) {
-		
-		Issue issue = this.issueRepo.findByIssueName(issueName);
-		IssueDTO issueDTO = new IssueDTO();
-		issueDTO.setQuestions(convertQuestionsToStrings(issue.getQuestions()));
-		return issueDTO;
-	}
-	
-	
 	private List<IssueDTO> convertIssuesFromCategoryToString(Collection<Issue> issues) {
 		
 		List<IssueDTO> issueList = new ArrayList<IssueDTO>();
 		for (Issue issue : issues) {
 			IssueDTO issueDTO = new IssueDTO();
+			issueDTO.setId(issue.getIssueId());
 			issueDTO.setIssueName(issue.getIssueName());
 			issueDTO.setissueDescription(issue.getIssueDescription());
 			issueList.add(issueDTO);
@@ -97,50 +97,228 @@ public class IssueService implements IIssueService{
 		return issueList;
 	}
 	
-	private List<String> convertQuestionsToStrings(Collection<Question> questions) {
+	public String getQuestionsByIssue(Long issueId) {
 		
-		List<String> questionStrings = new ArrayList<String>();
-		for (Question question : questions) {
-			questionStrings.add(question.getQuestion());
+		Issue issue = this.issueRepo.findByIssueId(issueId);
+		List<QuestionDTO> questions = new ArrayList<QuestionDTO>();
+				
+		for (Question question : issue.getQuestions()) {	
+			QuestionDTO questionDTO = new QuestionDTO();
+			questionDTO.setId(question.getQuestionID());
+			questionDTO.setQuestionText(question.getQuestion());
+			questions.add(questionDTO);	
 		}
-		return questionStrings;
+
+		return WebUtils.generateJSON(questions);
+	}
+		
+	public String addCategory(String category) {
+		
+		feedback.setId(generateCategory(category));
+		return WebUtils.generateJSON(feedback);
 	}
 	
-	public void generateIssues() {
-		generateCategories();
-		Issue issue = new Issue();
-		issue.setIssueName("issue1");
-		issue.setIssueDescription("Description for issue1");
-		issue.setIssueCategory(issueCategoryRepo.findByCategoryName("Category1"));
-		issueRepo.save(issue);
-		generateQuestions(issue);
+	public String addIssueToCategory(Long catId, String issueName, String issueDescription) {
+		
+		feedback.setId(generateIssue(catId, issueName, issueDescription));
+		return WebUtils.generateJSON(feedback);
 	}
 	
-	private void generateCategories() {
+	public String addQuestionToIssue(Long issueId, String questionText) {
+		
+		feedback.setId(generateQuestion(issueId, questionText));
+		return WebUtils.generateJSON(feedback);
+	}
+	
+
+	public void generateTemplates() {
+		
+		Long cat1Id = generateCategory("Category1");
+		Long cat2Id = generateCategory("Category2");
+		Long cat3Id = generateCategory("Category3");
+		
+		Long issue1Id = generateIssue(cat1Id, "issue1", "issue1 description goes here");
+		Long issue2Id = generateIssue(cat1Id, "issue2", "issue2 description goes here");
+		Long issue3Id = generateIssue(cat1Id, "issue3", "issue3 description goes here");
+		
+		generateIssue(cat2Id, "issue4", "issue1 description goes here");
+		generateIssue(cat2Id, "issue5", "issue2 description goes here");
+		generateIssue(cat2Id, "issue6", "issue3 description goes here");
+		
+		generateQuestion(issue1Id, "this is question 1");
+		generateQuestion(issue1Id, "this is question 2");
+		
+		generateQuestion(issue2Id, "this is question 1");
+		generateQuestion(issue2Id, "this is question 2");
+		//generateQuestion("this is question 3", "issue1");
+	}
+	
+	public Long generateCategory (String categoryName) {
 		
 		IssueCategory category = new IssueCategory();
-		category.setCategoryName("Category1");
-		category.setCategoryDescription("blablablablablablablablalba");
-		issueCategoryRepo.save(category);
-		
-		IssueCategory category2 = new IssueCategory();
-		category2.setCategoryName("Category2");
-		category2.setCategoryDescription("blablablablabladsadsadsadsadwqdq3323r32r32r23r23blablablalba");
-		issueCategoryRepo.save(category2);
-		System.out.println("CATEGORIES GENERATED SUCCESSFULLY");
+		category.setCategoryName(categoryName);
+		try {
+			category = issueCategoryRepo.save(category);				
+		}				
+		catch (Exception e) {
+			feedback.setMsg("Error generating category: " +e);
+			return (long) -1;
+		}
+		feedback.setMsg("Successfully generated category: ");
+		return category.getCategoryId();
 	}
 	
-	private void generateQuestions(Issue issue) {
-		Question question1 = new Question();
-		question1.setIssue(issue);
-		question1.setQuestion("Issue1_Question1");
-		Question question2 = new Question();
-		question2.setQuestion("Issue1_Question2");
-		question2.setIssue(issue);
-		questionRepo.save(question1);
-		questionRepo.save(question2);
+	public String updateCategory (Long id, String catName, String catNameChange) {
 		
-		System.out.println("QUESTIONS GENERATED SUCCESSFULLY");
+		IssueCategory catModel = issueCategoryRepo.findByCategoryId(id);
+		catModel.setCategoryName(catNameChange);
+		
+		feedback.setName(catName);
+		feedback.setNameChange(catNameChange);
+
+		try {
+			issueCategoryRepo.save(catModel);				
+		}				
+		catch (Exception e) {
+			feedback.setMsg("Error: " +e);
+			return WebUtils.generateJSON(feedback);
+		}		
+		feedback.setMsg("Successfully updated category: " +catName +" to " +catNameChange);
+		return WebUtils.generateJSON(feedback);
 	}
 	
+	public String deleteCategory(Long id) {
+		
+		IssueCategory catModel = issueCategoryRepo.findByCategoryId(id);
+		
+		feedback.setName(catModel.getCategoryName());
+			
+		try {
+			issueCategoryRepo.delete(catModel);			
+		}				
+		catch (Exception e) {
+			feedback.setMsg("Error: " +e);
+			return WebUtils.generateJSON(feedback);
+		}
+		
+		feedback.setMsg("Successfully deleted");
+		return WebUtils.generateJSON(feedback);
+	}	
+	
+	private Long generateIssue (Long categoryId, String issueName, String issueDescription) {
+		
+		IssueCategory category = issueCategoryRepo.findByCategoryId(categoryId);
+		
+		Issue issue = new Issue();
+		issue.setIssueName(issueName);
+		issue.setIssueDescription(issueDescription);
+		issue.setIssueCategory(category);
+		
+		List<Question> questions = new ArrayList<Question>();
+		issue.setQuestions(questions);
+		
+		try {
+			issue = issueRepo.save(issue);			
+		}				
+		catch (Exception e) {
+			feedback.setMsg("Error generating issue: " +e);
+			return (long) -1;
+		}
+
+		feedback.setMsg("Successfully added issue to category: " +category.getCategoryName());
+		return issue.getIssueId();
+	}
+	
+	public String updateIssue(Long id, String issueName, String issueDescription) {
+		
+		Issue issue = issueRepo.findByIssueId(id);
+		issue.setIssueName(issueName);
+		issue.setIssueDescription(issueDescription);
+		
+		try {
+			issue = issueRepo.save(issue);		
+		}				
+		catch (Exception e) {
+			feedback.setMsg("Error updating issue: " +e);
+			 WebUtils.generateJSON(feedback);
+		}
+		
+		feedback.setMsg("Successfully updated issue: ");
+		return WebUtils.generateJSON(feedback);
+	}
+	
+	public String deleteIssue(Long id) {
+		
+		Issue issue = issueRepo.findByIssueId(id);
+		
+		feedback.setName(issue.getIssueName());
+		
+		try {
+			issueRepo.delete(issue);			
+		}				
+		catch (Exception e) {
+			feedback.setMsg("Error: " +e);
+			return WebUtils.generateJSON(feedback);
+		}
+		
+		feedback.setMsg("Successfully deleted");
+		return WebUtils.generateJSON(feedback);
+	}
+		
+	private Long generateQuestion (Long issueId, String questionText) {
+		
+		Issue issue = issueRepo.findByIssueId(issueId);
+						
+		Question question = new Question();
+		question.setIssue(issue);
+		question.setQuestion(questionText);	
+		issue.addQuestion(question);
+		
+		try {
+			question = questionRepo.save(question);		
+		}				
+		catch (Exception e) {
+			feedback.setMsg("Error generating question: " +e);
+			return (long) -1;
+		}
+		issueRepo.save(issue);
+		
+		
+		return question.getQuestionID();
+	}
+	
+	public String updateQuestion(Long id, String questionChange) {
+		
+		Question question = questionRepo.findByQuestionId(id);		
+		question.setQuestion(questionChange);
+		
+		try {
+			question = questionRepo.save(question);		
+		}				
+		catch (Exception e) {
+			feedback.setMsg("Error updating question: " +e);
+			 WebUtils.generateJSON(feedback);
+		}
+		
+		feedback.setMsg("Successfully updated question: ");
+		return WebUtils.generateJSON(feedback);
+	}
+	
+	public String deleteQuestion(Long id) {
+		
+		Question question = questionRepo.findByQuestionId(id);
+		
+		try {
+			questionRepo.delete(question);			
+		}				
+		catch (Exception e) {
+			feedback.setMsg("Error: " +e);
+			return WebUtils.generateJSON(feedback);
+		}
+		
+		feedback.setMsg("Successfully deleted");
+		return WebUtils.generateJSON(feedback);
+		
+	}
+		
 }
