5222d0b358ec3dc01f11cb4977fc72aa1dbd4eee
kenerstad <kenerstad2@gmail.com>
2019-04-09 21:15:05 +0200

    ALTER getClientBrowser()
    Removed identical code to getClientOS(), added new code to retrieve browser info.

--- src/main/java/net/helpdeskhelper/helpdeskhelper/service/ClientInfoService.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/service/ClientInfoService.java
@@ -2,22 +2,23 @@ package net.helpdeskhelper.helpdeskhelper.service;
 
 //## Java
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-//## Project
-import net.helpdeskhelper.helpdeskhelper.utils.WebUtils;
-
 //## Spring
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
+//## Project
+import net.helpdeskhelper.helpdeskhelper.utils.WebUtils;
 
 
 @Service
@@ -26,19 +27,23 @@ public class ClientInfoService implements IClientInfoService{
 	@Value("${geoLoc.APIKey}")
 	private String geoLocAPIKey;
 
-	public String getClientInfo(HttpServletRequest request) throws IOException{
+	public List<String> getClientInfo(HttpServletRequest request) throws IOException{
 		
 		//String ip = getClientIP(request);
-		String geoLocInfo = getClientGeolocationInfoIPv4("84.212.9.174");
-
-		System.out.println(request.getHeader("User-Agent"));
-		geoLocInfo.concat("   USER AGENT: " +request.getHeader("User-Agent"));
+		//String geoLocInfo = getClientGeolocationInfoIPv4("84.212.9.174");
+		String geoLocInfo = "here goes the geoloc info.";
+		List<String> clientInfo = new ArrayList<String>();
+		clientInfo.add("Browser: " +getClientBrowser(request));
+		clientInfo.add("OS: " +getClientOS(request));
+		clientInfo.add(geoLocInfo);
 		
-		return geoLocInfo;
+		return clientInfo;
 	}
+	
+
 			
 	// Checks for proxy forwarded IP adress, if none, gets actual IP address.
-	public String getClientIP(HttpServletRequest request) {
+	private String getClientIP(HttpServletRequest request) {
 		
 		String ipAddress = request.getHeader("x-forwarded-for");
         if (ipAddress == null) {
@@ -49,28 +54,56 @@ public class ClientInfoService implements IClientInfoService{
         return ipAddress;
 	}	
 	
-	public String getClientBrowser(HttpServletRequest request) {
+	private String getClientBrowser(HttpServletRequest request) {
 		
 	     final String browserDetails = request.getHeader("User-Agent");
+	     
+	        String  userAgent       =   browserDetails;
+	        String  user            =   userAgent.toLowerCase();
 
-	        //=================OS=======================
-	        final String lowerCaseBrowser = browserDetails.toLowerCase();
-	        if (lowerCaseBrowser.contains("windows")) {
-	            return "Windows";
-	        } else if (lowerCaseBrowser.contains("mac")) {
-	            return "Mac";
-	        } else if (lowerCaseBrowser.contains("x11")) {
-	            return "Unix";
-	        } else if (lowerCaseBrowser.contains("android")) {
-	            return "Android";
-	        } else if (lowerCaseBrowser.contains("iphone")) {
-	            return "IPhone";
-	        } else {
-	            return "UnKnown, More-Info: " + browserDetails;
+	        String browser = "";
+
+	     if (user.contains("msie")) {
+	            String substring=userAgent.substring(userAgent.indexOf("MSIE")).split(";")[0];
+	            browser=substring.split(" ")[0].replace("MSIE", "IE")+"-"+substring.split(" ")[1];
+	        } 
+	     
+	     else if (user.contains("safari") && user.contains("version")) {
+	            browser=(userAgent.substring(userAgent.indexOf("Safari")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+	        } 
+	     
+	     else if ( user.contains("opr") || user.contains("opera")) {
+	            if(user.contains("opera"))
+	                browser=(userAgent.substring(userAgent.indexOf("Opera")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+	            else if(user.contains("opr"))
+	                browser=((userAgent.substring(userAgent.indexOf("OPR")).split(" ")[0]).replace("/", "-")).replace("OPR", "Opera");
+	        } 
+	     
+	     else if (user.contains("chrome")) {
+	            browser=(userAgent.substring(userAgent.indexOf("Chrome")).split(" ")[0]).replace("/", "-");
+	        } 
+	     
+	     else if ((user.indexOf("mozilla/7.0") > -1) || (user.indexOf("netscape6") != -1)  || (user.indexOf("mozilla/4.7") != -1) || (user.indexOf("mozilla/4.78") != -1) || (user.indexOf("mozilla/4.08") != -1) || (user.indexOf("mozilla/3") != -1) ) {
+	            //browser=(userAgent.substring(userAgent.indexOf("MSIE")).split(" ")[0]).replace("/", "-");
+	            browser = "Netscape-?";
+
+	        } 
+	     
+	     else if (user.contains("firefox")) {
+	            browser=(userAgent.substring(userAgent.indexOf("Firefox")).split(" ")[0]).replace("/", "-");
+	        } 
+	     
+	     else if(user.contains("rv")) {
+	            browser="IE-" + user.substring(user.indexOf("rv") + 3, user.indexOf(")"));
+	        } 
+	     else {
+	            browser = "UnKnown, More-Info: "+userAgent;
 	        }
+	     
+	     return browser;
 	}
 	
-	public String getClientOS(HttpServletRequest request) {
+	private String getClientOS(HttpServletRequest request) {
 		
 		final String browserDetails = request.getHeader("User-Agent");
 
@@ -92,7 +125,7 @@ public class ClientInfoService implements IClientInfoService{
 	}
 	
 	// Gets domain name from servlet request.
-	public String getDomainFromServletRequest(HttpServletRequest request) {
+	private String getDomainFromServletRequest(HttpServletRequest request) {
 
 		String domain = request.getRemoteHost();		
 		if(domain == null)
@@ -101,7 +134,7 @@ public class ClientInfoService implements IClientInfoService{
 	}
 	
 	// Queries geolocation API on IP - API documentation: https://ipdata.co/docs.html
-	public String getClientGeolocationInfoIPv4(String ip) throws IOException{
+	private String getClientGeolocationInfoIPv4(String ip) throws IOException{
 		
 		System.out.println(geoLocAPIKey);
 		String geoURL = "https://api.ipdata.co/" +ip +"?api-key=" +geoLocAPIKey;
@@ -113,11 +146,15 @@ public class ClientInfoService implements IClientInfoService{
 
 		//System.out.println("body:" + response.readEntity(String.class));
 		
-		String rawClientInfo = response.readEntity(String.class);
-		
-		String cleanedClientInfo = rawClientInfo.replace("{", "").replace("}", "")
+		String rawClientGeoLocInfo = response.readEntity(String.class);
+	
+		String cleanedClientInfo = rawClientGeoLocInfo.replace("{", "").replace("}", "")
 				.replace("[", "").replace("]", "").replace("\"", "");
 		
+	
+	
+		
 		return cleanedClientInfo;
+		//return cleanedClientInfo;
 	}	
 }
