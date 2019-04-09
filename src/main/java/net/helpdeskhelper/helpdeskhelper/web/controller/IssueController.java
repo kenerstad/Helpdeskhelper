822094292ce801a9baba524c92deee93cc848469
kenerstad <kenerstad2@gmail.com>
2019-04-09 21:39:00 +0200

    ALTER add imports, add methods
    Added: getQuestionsFromIssues(), submitHelperForm().

--- src/main/java/net/helpdeskhelper/helpdeskhelper/web/controller/IssueController.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/web/controller/IssueController.java
@@ -1,7 +1,10 @@
 package net.helpdeskhelper.helpdeskhelper.web.controller;
+import java.io.IOException;
 // ## Java
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 // ## Spring
 import org.springframework.ui.Model;
@@ -10,10 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-// ## Project
+// ## Local
 import net.helpdeskhelper.helpdeskhelper.service.IIssueService;
+import net.helpdeskhelper.helpdeskhelper.service.IMailService;
 import net.helpdeskhelper.helpdeskhelper.web.dto.IssueDTO;
-
+import net.helpdeskhelper.helpdeskhelper.service.IClientInfoService;
+import net.helpdeskhelper.helpdeskhelper.utils.WebUtils;
 
 
 
@@ -23,6 +28,12 @@ public class IssueController {
 	@Autowired
 	IIssueService issueService; 
 	
+	@Autowired
+	IClientInfoService clientInfoService;
+	
+	@Autowired
+	IMailService mail;
+	
 	
 	@GetMapping("/api/issue/getallcategories")
 	public String getAllCategories(Model model) {
@@ -30,10 +41,47 @@ public class IssueController {
 		return issueService.getIssueCategories();			
 	}
 	
-	@PostMapping("/api/issue/getIssuesFromCategory")
-	public String getIssuesFromCategory(@RequestParam("category") String category) {
+	@PostMapping("/api/issue/getissuesfromcategory")
+	public String getIssuesFromCategory(@RequestParam("id") Long catId) {
 		
-		return issueService.getIssuesByCategory(category);
+		return issueService.getIssuesByCategory(catId);
 		
 	}
+	
+	@PostMapping("/api/issue/getquestionsfromissue")
+	public String getQuestionsFromIssues(@RequestParam("issueId") Long issueId) {
+		
+		
+		return issueService.getQuestionsByIssue(issueId);
+		
+	}
+	
+	@PostMapping("/api/helper/submithelperform")
+	public String submitHelperForm(
+			@RequestParam("phoneNumber") String phoneNumber, 
+			@RequestParam("issue") String issue,
+			@RequestParam("questionsAndAnswers") String questionsAnd,
+			HttpServletRequest req) throws IOException {
+		
+		List<String> clientInfo = clientInfoService.getClientInfo(req);
+	
+		@SuppressWarnings("unchecked")
+		List<String> a = (ArrayList<String>) WebUtils.decodeJSON(questionsAnd);
+		
+		List<String> userInfo = new ArrayList<String>();
+		
+		userInfo.add("Phone number: " +phoneNumber);
+		userInfo.add("Issue: " +issue);
+		userInfo.addAll(a);
+		userInfo.addAll(clientInfo);
+		
+		System.out.println(userInfo);
+		
+		String userInfoJSON = WebUtils.generateJSON(userInfo);
+		
+		mail.sendMessageToSupport(userInfo);
+		
+		return userInfoJSON;
+	}
+	
 }
