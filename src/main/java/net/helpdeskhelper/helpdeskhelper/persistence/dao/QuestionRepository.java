2139a0a87a492fba3f5cf91e4c799f8d5b9923e8
kenerstad <kenerstad2@gmail.com>
2019-04-09 21:05:45 +0200

    ALTER findByQuestionId() add

--- src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/QuestionRepository.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/QuestionRepository.java
@@ -1,9 +1,13 @@
 package net.helpdeskhelper.helpdeskhelper.persistence.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import net.helpdeskhelper.helpdeskhelper.persistence.model.IssueCategory;
 import net.helpdeskhelper.helpdeskhelper.persistence.model.Question;
 
 public interface QuestionRepository extends CrudRepository<Question, Integer>{
 
+	public Question findByQuestionId(Long id);
 }
