882bf12f73f0ec67c96445d002ecfbb342ef8eb0
kenerstad <kenerstad2@gmail.com>
2019-04-09 21:09:37 +0200

    ALTER formatting, indentation, add directives to relational model/Issue

--- src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/model/Issue.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/model/Issue.java
@@ -1,11 +1,16 @@
 package net.helpdeskhelper.helpdeskhelper.persistence.model;
 
+import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -20,18 +25,27 @@ public class Issue {
     @GeneratedValue
     @NotNull
     private Long issueId;
-	
-	@NotNull
-	@Size(max = 36)
-	private String issueCategory;
-	
+		
 	@NotNull
 	@Size(max = 36)
 	private String issueName;
 	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "issue")
-	private Set<Question> questions;
+	@NotNull
+	@Size(max = 150)
+	private String issueDescription;
+		
 
+	//## Relational	
+	@OneToMany(
+			cascade = CascadeType.REMOVE, 
+			orphanRemoval = true, 
+			fetch = FetchType.LAZY, 
+			mappedBy = "issue")
+	private List<Question> questions;
+	@ManyToOne
+	@JoinColumn(name="categoryId", nullable=false)
+	private IssueCategory issueCategory;
+	
 	
 	public Long getIssueId() {
 		return issueId;
@@ -40,15 +54,7 @@ public class Issue {
 	public void setIssueId(Long issueId) {
 		this.issueId = issueId;
 	}
-
-	public String getIssueCategory() {
-		return issueCategory;
-	}
-
-	public void setIssueCategory(String issueCategory) {
-		this.issueCategory = issueCategory;
-	}
-
+	
 	public String getIssueName() {
 		return issueName;
 	}
@@ -56,16 +62,34 @@ public class Issue {
 	public void setIssueName(String issueName) {
 		this.issueName = issueName;
 	}
+	
+	public String getIssueDescription() {
+		return issueDescription;
+	}
 
-	public Set<Question> getQuestions() {
+	public void setIssueDescription(String issueDescription) {
+		this.issueDescription = issueDescription;
+	}
+
+	public IssueCategory getIssueCategory() {
+		return issueCategory;
+	}
+
+	public void setIssueCategory(IssueCategory issueCategory) {
+		this.issueCategory = issueCategory;
+	}
+
+	public List<Question> getQuestions() {
 		return questions;
 	}
 
-	public void setQuestions(Set<Question> questions) {
+	public void setQuestions(List<Question> questions) {
 		this.questions = questions;
 	}
 	
-	
+	public void addQuestion(Question question) {
+		questions.add(question);
+	}
 	
 }
 
