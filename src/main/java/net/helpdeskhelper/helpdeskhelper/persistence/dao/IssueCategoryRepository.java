2591f7d31bdd814f1a8297c86f8cb7af7a1a61c4
kenerstad <kenerstad2@gmail.com>
2019-04-09 21:03:53 +0200

    ALTER findByCategoryId() add

--- src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/IssueCategoryRepository.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/IssueCategoryRepository.java
@@ -1,11 +1,13 @@
 package net.helpdeskhelper.helpdeskhelper.persistence.dao;
 
-import net.helpdeskhelper.helpdeskhelper.persistence.model.Issue;
 import net.helpdeskhelper.helpdeskhelper.persistence.model.IssueCategory;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface IssueCategoryRepository extends CrudRepository<IssueCategory, Integer>{
 
 	public IssueCategory findByCategoryName(String name);
+	public IssueCategory findByCategoryId(Long id);
 }
