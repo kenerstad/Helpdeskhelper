99969d2860eaacedd65266e09ecb928ed83ab470
kenerstad <kenerstad2@gmail.com>
2019-04-09 21:04:57 +0200

    ALTER findByIssueId() add

--- src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/IssueRepository.java
+++ src/main/java/net/helpdeskhelper/helpdeskhelper/persistence/dao/IssueRepository.java
@@ -1,11 +1,16 @@
 package net.helpdeskhelper.helpdeskhelper.persistence.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import net.helpdeskhelper.helpdeskhelper.persistence.model.Issue;
 
+@Transactional
 public interface IssueRepository extends CrudRepository<Issue, Integer>{
 
 	public Issue findByIssueName(String name);
-	public Issue findByIssueCategory(String category);
+	public Issue findByIssueId(Long id);
+	public List<Issue> findAllByIssueCategory(String category);
 }
